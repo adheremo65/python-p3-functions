cb498f06203eb88375016c73d8d08cc5de887954
Abdela Umer <adheremo65@gmail.com>
2023-07-06 13:08:05 -0400

    function python lab

--- lib/functions.py
+++ lib/functions.py
@@ -6,8 +6,11 @@ def greet_programmer():
 def greet(name):
     print(f"Hello, {name}!")
 
+
 def greet_with_default(name="programmer"):
-    print(f"hello,{name}")
+    print(f"Hello,{name}!")
+def greet_with_default(name="programmer"):
+    print(f"Hello, {name}!")    
 
 def add(num1, num2):
     return num1 + num2
